470758a1a3788f402a53e3cfc154aba975c3b102
camilothomas <cembre@gmail.com>
2020-01-26 16:04:23 -0500

    made random color changing blobs

--- src/App.js
+++ src/App.js
@@ -1,35 +1,40 @@
 import React from 'react';
 import './App.css';
 import Blob from "./Blob";
-let jitters = false;
 
-function handleBs(event) {
-  jitters = true;
-}
-
-const element = (
-  <div className="flexMe">
-    <Blob className="demDivs"  />
-    <Blob className="repubDivs" />
-    <Blob className="marlboroDivs" />
-  </div>
-);
-
-const elementTwo = (
-<div className="flexMe">
-    <Blob className="repubDivs" onClick={handleBs}/>
-    <Blob className="demDivs" />
-    <Blob className="repubDivs" />
-  </div>
-);
+const BLOB_CLASSES = ['repubDivs', 'demDivs', 'marlboroDivs'];
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            colorOrder: {
+                first: "repubDivs",
+                second: "demDivs",
+                third: "repubDivs",
+            }
+        };
+    }
+   
+    handleBs = () => {
+        this.setState(
+            {
+                colorOrder: {
+                    first: BLOB_CLASSES[Math.floor(Math.random() * 3)],
+                    second: BLOB_CLASSES[Math.floor(Math.random() * 3)],
+                    third: BLOB_CLASSES[Math.floor(Math.random() * 3)],
+                }
+            } 
+        );  
+        console.log('don\'t think I\'ll see you');
+    }
+    
     render() {
         return (
             <div className="flexMe">
-                <Blob className="repubDivs" onClick={handleBs}/>
-                <Blob className="demDivs" />
-                <Blob className="repubDivs" />
+                <Blob className={this.state.colorOrder.first} onClick={this.handleBs}/>
+                <Blob className={this.state.colorOrder.second} onClick={this.handleBs}/>
+                <Blob className={this.state.colorOrder.third} onClick={this.handleBs}/>
             </div>
         );
     }
