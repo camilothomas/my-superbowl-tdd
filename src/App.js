6123187e51e590e34961f2b87d5f42196eed2214
camilothomas <cembre@gmail.com>
2020-01-26 13:42:19 -0500

    moved logic from index.js to App.js not working yet

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import './App.css';
+let jitters = false;
 
+function handleBullshit(event) {
+  jitters = true;
+}
+
+function Blob(props) {
+  return <div className={props.className} onClick={props.onClick}></div>;
+}
+
+const element = (
+  <div className="flexMe">
+    <Blob className="demDivs"  />
+    <Blob className="repubDivs" />
+    <Blob className="marlboroDivs" />
+  </div>
+);
+
+const elementTwo = (
+<div className="flexMe">
+    <Blob className="repubDivs" onClick={handleBullshit}/>
+    <Blob className="demDivs" />
+    <Blob className="repubDivs" />
+  </div>
+);
 function App() {
-  return (
-    <>
-    <h1>App</h1>
-    <p>learn react</p>
-    </>
-  );
+  if (jitters) return (element);
+  else return (elementTwo);
 }
 
 export default App;
